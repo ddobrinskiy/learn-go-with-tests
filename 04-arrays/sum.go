dcc2cc1aee9e40bc5abba75d95bfa7fcdb84a1e7
David <david.dobrinskiy@gmail.com>
2020-02-22 14:56:05 +0300

    finish arrays lesson

--- 04-arrays/sum.go
+++ 04-arrays/sum.go
@@ -13,3 +13,22 @@ func SumAll(numbersToSum ...[]int) (sums []int) {
 	}
 	return
 }
+
+
+
+func SumAllTails(numbersToSum ...[]int) (sumsOfTails []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sumsOfTails = append(sumsOfTails, 0)
+		} else {
+			// slice numbers[1:] to exclude first number (definition of a TAIL)
+			tail := numbers[1:]
+			sumsOfTails = append(sumsOfTails, Sum(tail))
+		}
+
+	}
+	return
+}
+
+
+
