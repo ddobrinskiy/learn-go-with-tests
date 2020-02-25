b45843df6f876f498999f2a27fb79f13776c13e4
David Dobrinskiy <ddobrinskiy@ozon.ru>
2020-02-25 19:52:30 +0300

    finish pointers lesson

--- 05-structs/shapes_test.go
+++ 05-structs/shapes_test.go
@@ -39,9 +39,9 @@ func TestAreaOld(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		name string
-		shape Shape
-		hasArea  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
 		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
 		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
@@ -58,5 +58,4 @@ func TestArea(t *testing.T) {
 		})
 	}
 
-	}
-
+}
